fdcfe33a8b7257606b43efed71cf296a64f81db1
sayen512 <kavinariasa94@gmail.com>
2024-05-12 12:54:55 -0600

    Actualizacion de base e datos tablas y un agregar.xml

---
+++ ProyectoPDM/app/src/main/java/sv/edu/ues/fia/pdm/proyecto/grupo3/ui/solicitudes/AgregarSolicitudesHorarioActivity.java
@@ -0,0 +1,16 @@
+package sv.edu.ues.fia.pdm.proyecto.grupo3.ui.solicitudes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sv.edu.ues.fia.pdm.proyecto.grupo3.R;
+
+public class AgregarSolicitudesHorarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_solicitudes_horario);
+    }
+}
